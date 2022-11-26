682bf29b16982c864dd71b9b4d380292a36adbce
Тоша <zzzzlzzzz@yandex.com>
2022-11-26 19:48:04 +0400

    fix slow mode bar

--- Assets/Scripts/UI/SlowModeGroupController.cs
+++ Assets/Scripts/UI/SlowModeGroupController.cs
@@ -6,32 +6,8 @@ public class SlowModeGroupController : MonoBehaviour
 {
     public FishSlowModeController fishSlowModeController;
     public GameObject slowModeBar;
-    public GameObject slowModeButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (fishSlowModeController.enabled)
-        {
-            slowModeBar.SetActive(true);
-            if (fishSlowModeController.CanGoToSlowMode())
-            {
-                //slowModeButton.SetActive(true);
-            }
-            else
-            {
-              //  slowModeButton.SetActive(false);
-            }
-        }
-        else
-        {
-            slowModeBar.SetActive(false);
-            //slowModeButton.SetActive(false);
-        }
+        slowModeBar.SetActive(fishSlowModeController.enabled);
     }
 }
